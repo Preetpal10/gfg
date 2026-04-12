f55e34ddc6c10b07a3019e9c3479ec76f45e5a1f
Preetpal10 <preetpal0107@gmail.com>
2026-04-12 22:50:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -1,10 +1,8 @@
 class Solution {
-    int factorial(int n) {
+    public int factorial(int n) {
         int factorial = 1;
-        for (int i =1 ; i <=n; i++){
-            
+        for (int i = 1; i <= n; i++){
         factorial = factorial * i;
-        
         }
         return factorial;
     }
