aad64f6822382313320ec1f8dff004a2157dae37
Preetpal10 <preetpal0107@gmail.com>
2026-04-16 20:48:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else Statement/the-else-statement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       
+      
+    if( n > 100){
+           System.out.println("Big");
+    }else{
+           System.out.println("Small");
+    }
+        
+    }
+}
