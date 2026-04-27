8e228b2fe25517fc6a6ce381d0e2f7a1cc01332a
Preetpal10 <preetpal0107@gmail.com>
2026-04-27 19:55:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/The If Statement/the-if-statement.java
+++ Difficulty: Basic/The If Statement/the-if-statement.java
@@ -4,11 +4,11 @@ public class Solution {
     public static void solve() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        // check whether n is big or a number
-        if( n > 100){
-             System.out.println("Big");
+        if( n> 100){
+            System.out.println("Big");
+            
         }
+             System.out.println("Number");
         
-        System.out.println("Number");
     }
 }
