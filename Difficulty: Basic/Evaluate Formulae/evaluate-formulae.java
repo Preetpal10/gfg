353e27bd942f33ef461bbcdba15cbd1c94c64fd9
Preetpal10 <preetpal0107@gmail.com>
2026-04-27 20:01:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Evaluate Formulae/evaluate-formulae.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int calculate(int a, int b, int c, int d) {
+        // code here
+        int t = a + b ;
+        int r = t / c;
+        int q = r +d;
+        return q;
+    }
+}
