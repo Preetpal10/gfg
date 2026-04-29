86991fd3b059985d7f0c29740d1b428ed52845b9
Preetpal10 <preetpal0107@gmail.com>
2026-04-29 21:27:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input Integer/input-integer.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        //  Take input
+        //  Add 10 and Pri
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int a = n +10;
+        System.out.println(a);
+    }
+}
